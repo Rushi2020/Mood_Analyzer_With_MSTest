04ee45751dd9686e1168dde15c5888d642047a2e
Rushi <189489rishi@gmail.com>
2022-03-08 13:45:33 +0530

    Add project files.

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyzer_With_MSTest
+{
+    public class MoodAnalyzer {
+         
+        // string message = "SAD";
+            public string AnalyzeMood(string mood)
+            {
+                if (mood.ToLower().Contains("sad"))
+                    return "sad";
+                else
+                    return "happy";
+            }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+ namespace Mood_Analyzer_With_MSTest
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzing Program");
+        }
+    }
+}
